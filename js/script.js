cbe376b9950dbf9cbbca6bdbdbe9023b79ac39e7
ALAM-97 <alexamara97@gmail.com>
2021-08-26 17:33:07 +0200

    FIX - JS

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 var userSurname = prompt("Inserisci il tuo cognome");
 // 2. Inseriscilo in un array con altri cognomi.
 var allSurnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-allSurnames.push(userSurname);
+allSurnames.push(userSurname[0].toUpperCase() + userSurname.slice(1));
 // 3. Stampa la lista in ordine alfabetico.
 console.log(allSurnames.sort());
-console.log(allSurnames.indexOf(userSurname) + 1);
 // 4. Scrivi il n° della posizione del cognome nella lista.
+console.log(allSurnames.indexOf(userSurname) + 1);
